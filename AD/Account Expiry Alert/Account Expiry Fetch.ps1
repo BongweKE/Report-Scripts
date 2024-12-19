04eb3795f1399b2529b1268d8517fd7075e5cda7
BongweKE <bongwe.obi@gmail.com>
2024-12-19 14:10:58 +0300

    HR focal point csv updates?

--- AD/Account Expiry Alert/Account Expiry Fetch.ps1
+++ AD/Account Expiry Alert/Account Expiry Fetch.ps1
@@ -56,10 +56,6 @@ Prep for All Accounts
 --------------------------------------------------------------
 #>
 
-
-# NEXT STEP:
-# - Test this script for One department (Use OU to select/exclude)
-
 $selectObject = @('Name', 'samAccountName', 'ObjectClass', 'AccountExpirationDate', 'lastLogonDate', 'Enabled', 'PasswordNeverExpires')
 $ICRAF_ExcludedOU = @('ICRAF BOT','ICRAF Spouses','Disabled accounts','ICRAF Kenya General Accounts','ICRAF Administrators','ICRAF MFI','ICRAF Meeting Rooms','ICRAF ICT','ICRAF OCS','EC Regreening Project','ICRAF Sharepoint Users','ICRAF Kenya Shared Mailboxes','ICRAF Kenya Service Accounts') #update with KE data
 $ICRAFOU = 'OU=ICRAFHUB,DC=CIFOR-ICRAF,DC=ORG'
@@ -71,28 +67,33 @@ $ICRAFExcludedOURegex = ($ICRAF_ExcludedOU | ForEach-Object { [regex]::Escape($_
 [datetime]$In7Days = (Get-Date -Hour 0 -Minute 0 -Second 0).AddDays(7)
 [datetime]$In1Day = (Get-Date -Hour 0 -Minute 0 -Second 0).AddDays(1)
 
+<#------------- Sometimes I change date For Testing Purposes -------------------------------------#>
+# $today = (Get-Date).AddDays(-1)
+$today = Get-Date
+
 #Accounts expiring in 1 month
-$date_before_one_month = (Get-Date).AddDays(29).ToString("yyyy-MM-dd")
-$date_after_one_month = (Get-Date).AddDays(31).ToString("yyyy-MM-dd")
+
+$date_before_one_month = ($today).AddDays(29).ToString("yyyy-MM-dd")
+$date_after_one_month = ($today).AddDays(31).ToString("yyyy-MM-dd")
 $ICRAFExpiring_1Month =  Get-ADUser -SearchBase $ICRAFOU -Filter "Enabled -eq `$true -and AccountExpirationDate -gt '$date_before_one_month' -and AccountExpirationDate -lt '$date_after_one_month'" -Properties GivenName, Surname, UserPrincipalName, AccountExpirationDate, Manager, co | Where-Object { -not ($_.DistinguishedName -match $ICRAFExcludedOURegex) }
 
 #Accounts expiring in 2 weeks
-$date_before_two_weeks = (Get-Date).AddDays(13).ToString("yyyy-MM-dd")
-$date_after_two_weeks = (Get-Date).AddDays(15).ToString("yyyy-MM-dd")
+$date_before_two_weeks = ($today).AddDays(13).ToString("yyyy-MM-dd")
+$date_after_two_weeks = ($today).AddDays(15).ToString("yyyy-MM-dd")
 $ICRAFExpiring_2Weeks =  Get-ADUser -SearchBase $ICRAFOU -Filter "Enabled -eq `$true -and AccountExpirationDate -gt '$date_before_two_weeks' -and AccountExpirationDate -lt '$date_after_two_weeks'" -Properties GivenName, Surname, UserPrincipalName, AccountExpirationDate, Manager, co | Where-Object { -not ($_.DistinguishedName -match $ICRAFExcludedOURegex) }
 
 #Accounts expiring in 1 week
-$date_before_one_week = (Get-Date).AddDays(6).ToString("yyyy-MM-dd")
-$date_after_one_week = (Get-Date).AddDays(8).ToString("yyyy-MM-dd")
+$date_before_one_week = ($today).AddDays(6).ToString("yyyy-MM-dd")
+$date_after_one_week = ($today).AddDays(8).ToString("yyyy-MM-dd")
 $ICRAFExpiring_1Week = Get-ADUser -SearchBase $ICRAFOU -Filter "Enabled -eq `$true -and AccountExpirationDate -gt '$date_before_one_week' -and AccountExpirationDate -lt '$date_after_one_week'" -Properties GivenName, Surname, UserPrincipalName, AccountExpirationDate, Manager, co | Where-Object { -not ($_.DistinguishedName -match $ICRAFExcludedOURegex) }
 
 #Accounts expiring in 1 day
-$date_before_one_day = (Get-Date).ToString("yyyy-MM-dd")
-$date_after_one_day = (Get-Date).AddDays(2).ToString("yyyy-MM-dd")
+$date_before_one_day = ($today).ToString("yyyy-MM-dd")
+$date_after_one_day = ($today).AddDays(2).ToString("yyyy-MM-dd")
 $ICRAFExpiring_1Day = Get-ADUser -SearchBase $ICRAFOU -Filter "Enabled -eq `$true -and AccountExpirationDate -gt '$date_before_one_day' -and AccountExpirationDate -lt '$date_after_one_day'" -Properties GivenName, Surname, UserPrincipalName, AccountExpirationDate, Manager, co | Where-Object { -not ($_.DistinguishedName -match $ICRAFExcludedOURegex) }
 
 $Accounts_Nearing_Expiry = @()
-$hr_focal_point = 'w.wambugu@cifor-icraf.org'
+$hq_focal_point = 'w.wambugu@cifor-icraf.org'
 $kic_focal_point = 'I.Ingumba@cifor-icraf.org'
     
 <#
@@ -102,6 +103,7 @@ Action on each group
 #>
 
 $ICRAFExpiring_1Month | ForEach-Object {
+    $hr_focal_point = $hq_focal_point # Set Default to walter
     if ($_.co -ne $null) {
         foreach ($focal in $hr_focal_points_data) {
             foreach ($country in $focal."Country/Region" | ConvertFrom-Json) {
@@ -149,6 +151,7 @@ $ICRAFExpiring_1Month | ForEach-Object {
 }
 
 $ICRAFExpiring_2Weeks | ForEach-Object {
+    $hr_focal_point = $hq_focal_point # Set Default to walter
     if ($_.co -ne $null) {
         foreach ($focal in $hr_focal_points_data) {
             foreach ($country in $focal."Country/Region" | ConvertFrom-Json) {
@@ -195,6 +198,7 @@ $ICRAFExpiring_2Weeks | ForEach-Object {
 }
 
 $ICRAFExpiring_1Week | ForEach-Object {
+    $hr_focal_point = $hq_focal_point # Set Default to walter
     if ($_.co -ne $null) {
         foreach ($focal in $hr_focal_points_data) {
             foreach ($country in $focal."Country/Region" | ConvertFrom-Json) {
@@ -243,6 +247,7 @@ $ICRAFExpiring_1Week | ForEach-Object {
 
 
 $ICRAFExpiring_1Day | ForEach-Object {
+    $hr_focal_point = $hq_focal_point # Set Default to walter
     if ($_.co -ne $null) {
         # For Objects for which the Country Is Available, we use this info to choose Focal Point
         foreach ($focal in $hr_focal_points_data) {
@@ -290,5 +295,25 @@ $ICRAFExpiring_1Day | ForEach-Object {
     $Accounts_Nearing_Expiry += $accounts_expiring_in_1Day
 }
 
-# $Accounts_Nearing_Expiry | Select-Object EmailAddress, SupervisorEmailAddress, HrFocalPoint, AccountExpiry | Format-Table -AutoSize
+# $Accounts_Nearing_Expiry | Select-Object EmailAddress, SupervisorEmailAddress, HrFocalPoint, AccountExpiry, OU | Format-Table -AutoSize
+
 Send-AccountExpiryAlert -accounts_to_alert $Accounts_Nearing_Expiry
+
+
+<#
+-------------------------------------
+TESTING ONE USERS FOCAL POINT
+-------------------------------------
+$hr_focal_point_file_path = "C:\Users\lkadmin\OneDrive - CIFOR-ICRAF\Desktop\Auto Reports\Report Scripts\AD\Account Expiry Alert\HR Focal Points.csv"
+$hr_focal_points_data = Import-Csv -Path $hr_focal_point_file_path
+$x1 = Get-ADUser -Identity GReynoso -Properties co, Manager
+foreach ($focal in $hr_focal_points_data) {
+    foreach ($country in $focal."Country/Region" | ConvertFrom-Json) {
+        if ($country -eq $x1.co) {
+            $hr_focal_point = $focal.Email
+        }
+    }  
+}   
+
+$hr_focal_point
+#>
